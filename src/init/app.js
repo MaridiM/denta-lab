c47052ed0a3a9cacf473e3943b82f56b11a43763
MaridiM <maridim92@gmail.com>
2021-02-23 16:58:31 +0200

    Update babel -test server

--- src/init/app.js
+++ src/init/app.js
@@ -16,12 +16,12 @@ app.use(cors(corsOptions))
 
 
 // Static on  production build
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client', 'build')))
+// if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../../', 'client', 'build')))
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+        res.sendFile(path.join(__dirname, '../../', 'client', 'build', 'index.html'))
     })
-}
+// }
 
 // Endpoint
 export { app }
