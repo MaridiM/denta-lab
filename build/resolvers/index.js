24867f739f89cca72857be1384776b11fc7e9aba
MaridiM <maridim92@gmail.com>
2021-02-23 16:44:53 +0200

    Update babel

--- build/resolvers/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var _query = require("./query");
-
-module.exports.resolvers = {
-  Query: _query.Query
-};
-//# sourceMappingURL=index.js.map
--- build/resolvers/subscription.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=subscription.js.map
